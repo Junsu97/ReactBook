e79f396c9e3f4e0a82537b83749de25345515d18
Junsu97 <wnstn7759@naver.com>
2024-11-10 15:34:56 +0900

    Chapter 4 & Chapter 5

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,19 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-import { Fragment } from 'react';
-import MyComponent from './component/MyComponent';
-import Counter from './component/Counter';
-import Say from './component/Say';
+import { Component, Fragment } from 'react';
+// import MyComponent from './component/MyComponent';
+// import Counter from './component/Counter';
+// import Say from './component/Say';
+import EventPractice from './component/EvnetPractice';
+import FuncEventPracice from './component/FuncEventPractice';
+import ValidationSample from './component/ValidationSample';
+import ScrollBox from './component/ScrollBox';
 
-function App() {
-  const name = undefined;
+// function App() {
+class App extends Component {
+  // const name = undefined;
   //! Chapter 2
   // descriptions : Component와 props 설명 
   // return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>
   // descriptions : Class형 컴포넌트에서 state 설명 
   // return <Counter />;
   // descriptions : 함수형 컴포넌트에서 useState 설명 
-  return <Say />;
+
+  //! Chapter3
+  // return <Say />;
+
+  //! Chapter4
+  // return <EventPractice /> 
+  // return <FuncEventPracice />
+
+  //! Chapter5
+  // return <ValidationSample />
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => this.scrollBox = ref} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}> 밑으로</button>
+      </div >
+    )
+  }
 }
 
 export default App;
