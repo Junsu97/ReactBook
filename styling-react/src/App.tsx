c8888b2c66485de82d53a5069d125bd9b1f2ce3c
Junsu97 <wnstn7759@naver.com>
2024-12-15 13:28:11 +0900

    Chapter 9

--- styling-react/src/App.tsx
+++ styling-react/src/App.tsx
@@ -1,25 +1,13 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SassComponent from './components/SassComponent';
 
 function App() {
   const [value, setValue] = useState<number>(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SassComponent />
     </div>
   );
 }
